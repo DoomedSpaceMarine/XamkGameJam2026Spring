fe2774662545031678994bb41eea334b6761e14c
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2026-04-11 11:04:55 +0300

    projectile is destroyed if too far away

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -7,6 +7,11 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         this.transform.Translate(-projectileSpeed * Time.deltaTime, 0, 0);
+        //Making sure that the projectile is destroyed at some point.
+        if(this.transform.position.x <= -100)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
