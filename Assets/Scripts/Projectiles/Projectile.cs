bad891c5e2e956b95a9405af90d849fb837813f2
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2026-04-29 19:34:58 +0300

    projectile collision kills enemy

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -16,6 +16,10 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
         Destroy(gameObject);
     }
 }
